a20db7b9042aa89252d0c6fdeab9b0130a893ed9
David Barel <david@avalanche.codes>
2022-09-20 12:52:23 -0700

    Add main hero and Digital Ocean deployment script

--- src/layout/main-layout.js
+++ src/layout/main-layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from "../shared/header";
 import Footer from "../shared/footer";
 import { Outlet } from "react-router-dom";
+import MainHero from "../shared/hero/main-hero";
 
 const MainLayout = () => {
     return (
         <main>
             <Header />
+            <MainHero   />
             <div id={"content"} className={"container pt-4"}>
                 <Outlet />
             </div>
---
+++ src/shared/hero/main-hero.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './main-hero.scss';
+
+const MainHero = (props) => {
+    return (
+        <section className="pt-4 pt-md-11 bg-theme-dark pt-5">
+            <div className="container pt-5">
+                <div className="row align-items-center">
+                    <div className="col-12 col-md-5 col-lg-6 order-md-2">
+
+                        <img src="/main-hero.png"
+                             class="img-fluid mw-md-150 mw-lg-130 mb-6 mb-md-0" alt="..."
+                             data-aos="fade-up" data-aos-delay="100" />
+
+                    </div>
+                    <div className="col-12 col-md-7 col-lg-6 order-md-1" data-aos="fade-up">
+                        <h1 className="display-5 text-light text-center text-md-start">
+                            Welcome to <span className="text-primary">Cofflix</span><br />
+                           social, drink, watch.
+                        </h1>
+
+                        <p className="lead text-center text-md-start text-muted mb-6 mb-lg-8">
+                            Have a moment to spare? Come visit us and experience how cup of coffee
+                            and a good movie can transform your day!
+                        </p>
+
+                        <div className="text-center text-md-start">
+                            <a href="overview.html" className="btn btn-primary shadow lift me-1">
+                                View our menu.. <i className="fe fe-arrow-right d-none d-md-inline ms-3"></i>
+                            </a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div className=" bg-theme-dark  bottom-decor" />
+        </section>
+    );
+};
+
+export default MainHero;
